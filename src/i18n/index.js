0567344533441e74e9e5da0a930249c4ac431f76
John Leider <john.j.leider@gmail.com>
2018-10-22 00:51:44 -0400

    feat(language): move language, hook up lazy loading

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,30 +1,55 @@
-import { camelActual } from '@/util/helpers'
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import en from '@/lang/en'
-import * as langs from '@/lang'
+import axios from 'axios'
+import kebabCase from 'lodash/kebabCase'
 
 Vue.use(VueI18n)
 
-export function createI18n (ssrContext) {
-  let locale = 'en'
+export function createI18n (ssrContext, router) {
+  const loadedLanguages = {} // our default language that is pre-loaded
 
+  let locale
   if (ssrContext && ssrContext.lang) {
     locale = ssrContext.lang
   } else if (typeof document !== 'undefined') {
     locale = document.documentElement.lang
   }
 
-  const localePath = camelActual(locale)
-  const messages = { en }
+  const i18n = new VueI18n({
+    locale,
+    fallbackLocale: 'en'
+  })
 
-  if (locale !== 'en' && langs[localePath]) {
-    messages[locale] = langs[localePath]
+  function setI18nLanguage (lang) {
+    i18n.locale = lang
+    axios.defaults.headers.common['Accept-Language'] = lang
+
+    if (!ssrContext) {
+      document.querySelector('html').setAttribute('lang', lang)
+    }
+
+    return lang
   }
 
-  return new VueI18n({
-    locale: 'en',
-    fallbackLocale: 'en',
-    messages
+  function loadLanguageAsync (app, lang) {
+    if (!loadedLanguages[app]) loadedLanguages[app] = []
+
+    if (!loadedLanguages[app].includes(lang)) {
+      return import(/* webpackChunkName: "lang-[request]" */ `@/applications/${app}/lang/${lang}`).then(msgs => {
+        i18n.setLocaleMessage(lang, msgs.default)
+        loadedLanguages[app].push(lang)
+        return Promise.resolve(setI18nLanguage(lang))
+      })
+    }
+    return Promise.resolve(setI18nLanguage(lang))
+  }
+
+  router.beforeEach((to, from, next) => {
+    loadLanguageAsync(
+      kebabCase(to.name),
+      to.params.lang
+    ).then(() => next())
   })
+
+  return i18n
 }
